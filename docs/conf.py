a7356fa4a7c0700959c6e25ca0ceaabdf1fb3004
ffayalac <ffayalac@unal.edu.co>
2025-07-10 20:39:08 -0500

    testing docs

--- docs/conf.py
+++ docs/conf.py
@@ -35,4 +35,4 @@ napoleon_numpy_docstring = True
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_book_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
