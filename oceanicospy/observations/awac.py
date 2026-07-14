1b9feab38a508f84b4dc24663d54f419476ba23b
JuanDi <jtorresto@unal.edu.co>
2026-07-13 20:35:28 -0500

    En progreso: revision de lectura de datos SVC, libreria no nombra adecuadamente las columnas

--- oceanicospy/observations/awac.py
+++ oceanicospy/observations/awac.py
@@ -329,7 +329,7 @@ class AWAC:
         if from_single_wad:
             wad_filepath = wad_files[0]
             date_columns = ['month', 'day', 'year', 'hour', 'minute', 'second']
-            df = pd.read_csv(wad_filepath,sep=r"\s+",names=date_columns+list(column_names[2:]))
+            df = pd.read_csv(wad_filepath,sep=r"\s+",names=date_columns+list(column_names[6:]))
             df = df.dropna(axis=1)          
         else:
             burst_list = []
